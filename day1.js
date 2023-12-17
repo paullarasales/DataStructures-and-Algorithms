f1ca129c8ea35bcbac7cb956373aeb7a7af470ad
Paul <ahmadzufukli@gmail.com>
2023-12-17 13:24:46 +0800

    one down

--- day1.js
+++ day1.js
@@ -55,3 +55,26 @@ function findTheLongest(sentence) {
 const sentence = "Find the longest word in this sentence";
 const longestWord = findTheLongest(sentence);
 console.log(longestWord);
+
+// arranged the order
+
+function arrangeBySize(arr) {
+
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++ ) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        } 
+    }
+
+    return arr;
+}
+
+let arrayNums = [5, 7, 9, 2, 6];
+let sortedNums = arrangeBySize(arrayNums);
+console.log(sortedNums);
