dc269f5568cc214b71f8cea89e89b46b4b90f909
Paul <ahmadzufukli@gmail.com>
2024-08-01 20:28:23 +0800

    done with the problem number 2

---
+++ dayoftheweek.js
@@ -0,0 +1,31 @@
+function dayOfTheWeek(num) {
+    switch(num) {
+        case 1:
+            console.log('Monday');
+        break;
+        case 2:
+            console.log('Tuesday');
+        break;
+        case 3:
+            console.log('Wednesday');
+        break;
+        case 4:
+            console.log('Thursday');
+        break;
+        case 5:
+            console.log('Friday');
+        break;
+        case 6:
+            console.log('Saturday');
+        break;
+        case 7:
+            console.log('Sunday');
+        break;
+        default:
+            console.log('Invalid Input');
+        break
+    }
+}
+
+const num = 3;
+console.log(dayOfTheWeek(num));
