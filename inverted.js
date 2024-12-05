5bf22ad9ea2af56f06835911d3db67c3c3f77b0f
paullarasales <rkise2131@gmail.com>
2024-12-05 20:58:12 +0800

    fixed some minor issues

--- inverted.js
+++ inverted.js
@@ -1,7 +1,7 @@
 function inverted(n) {
-  for (let i = n; i > 0; i--) {
+ for (let i = n; i >= 0; i--) {
     let row = "";
-    for (let j = n; j >= i; j--) {
+    for (let j = 1; j <= i; j++) {
       row += j + " ";
     }
     console.log(row.trim());
